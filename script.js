399e7abf5abb96f5e4a71f268c079b3c28d74bd5
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-14 20:47:10 +0100

    Create the handleClick function

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const GameBoard = (() => {
-    let gameboard = ["", "", "", "", "", "", "", "", ""];
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
     const renderBoard = () => {
         let boardHTML = "";
-        gameboard.forEach((square, index) => {
+        gameBoard.forEach((square, index) => {
             boardHTML += `<div class="square" id="square-${index}">
                 ${square}
             </div>
             `
         });
+
         document.querySelector("#gameBoard").innerHTML = boardHTML;
+
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        });
+    }
+
+    const update = (index, value) => {
+        gameBoard[index] = value;
+        renderBoard();
     }
 
     return {
         renderBoard,
+        update,
     }
 })();
 
@@ -38,10 +50,22 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         GameBoard.renderBoard();
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+        });
+    }
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        GameBoard.update(index, players[currentPlayerIndex].mark);
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
     return {
         startGame,
+        handleClick,
     }
 })();
 
